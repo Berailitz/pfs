b6c10026446f8e9e64dd1e1c004d2524e5111d30
Berailitz <admin@ohhere.xyz>
2020-03-29 21:24:17 +0800

    feat(utility): use name pb

--- utility/utility.go
+++ utility/utility.go
@@ -3,12 +3,11 @@ package utility
 import (
 	"log"
 
-	"github.com/Berailitz/pfs/remotetree"
 	pb "github.com/Berailitz/pfs/remotetree"
 	"google.golang.org/grpc"
 )
 
-func BuildGCli(addr string, gopts []grpc.DialOption) (remotetree.RemoteTreeClient, error) {
+func BuildGCli(addr string, gopts []grpc.DialOption) (pb.RemoteTreeClient, error) {
 	log.Printf("build gcli: addr=%v", addr)
 	conn, err := grpc.Dial(addr, gopts...)
 	if err != nil {

4e8cbc530bf7b9904d423e0b59f20ae5b3b7c53f
Berailitz <admin@ohhere.xyz>
2020-04-04 21:44:54 +0800

    test(main): test dirs and rms

--- test.sh
+++ test.sh
@@ -2,12 +2,34 @@
 set -x
 mp='x'
 ls $mp
+
 mkdir $mp/a
-echo "dw" > $mp/a/z
-cat $mp/a/z
-touch $mp/b
-echo "xwdc" >> $mp/c
-cat $mp/b
-cat $mp/c
-tree $mp
+ls $mp
+
+echo "1stline" > $mp/a/text
+cat $mp/a/text
+
+echo "2ndline" > $mp/a/text
+echo "3rdline" >> $mp/a/text
+cat $mp/a/text
+
+mkdir $mp/a/adir
+ls -l $mp/a
+
+rmdir $mp/a/adir
+ls -l $mp/a
+
+touch $mp/empty
 ls -l $mp
+cat $mp/empty
+
+mkdir $mp/dir
+ls -l $mp
+
+rmdir $mp/dir
+ls -l $mp
+
+rm $mp/a/text
+ls -l $mp/a
+
+umount $mp

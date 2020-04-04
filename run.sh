69da637c4b755f3f2ddc4da9d61bee8924a072fe
Berailitz <admin@ohhere.xyz>
2020-04-04 23:37:27 +0800

    ci(run): use testCmd

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -x
-go run -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 --oneline --date=local HEAD)\"' -X 'main.buildTime=\"$(date '+%Y/%m/%d %H:%M:%S')\"'" main.go
+go build -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 --oneline --date=local HEAD)\"' -X 'main.buildTime=\"$(date '+%Y/%m/%d %H:%M:%S')\"'" && ./pfs -testCmd="./test.sh"

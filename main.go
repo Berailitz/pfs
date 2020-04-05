a743849e115fb5cdd6fa0e45685d62206b99e9c9
Berailitz <admin@ohhere.xyz>
2020-04-05 13:13:21 +0800

    feat(main): use dir as arg

--- main.go
+++ main.go
@@ -23,10 +23,6 @@ import (
 	"github.com/jacobsa/fuse"
 )
 
-const (
-	dir = "x"
-)
-
 var (
 	gitCommit string
 	buildTime string
@@ -101,15 +97,22 @@ func main() {
 	port := flag.Int("port", 10000, "The server port")
 	host := flag.String("host", "127.0.0.1", "The server host")
 	master := flag.String("master", "127.0.0.1:10000", "The master server addr")
+	dir := flag.String("dir", "", "Dir to mount.")
 	testCmd := flag.String("testCmd", "", "Script to test.")
 	testLog := flag.String("testLog", "testlog.txt", "Log file to write test output.")
 	flag.Parse()
 	ctx := context.Background()
 
+	if *dir == "" {
+		log.Fatalf("no dir specified, exit")
+	}
+
+	log.Printf("debug=%v", *debug)
+
 	localAddr := fmt.Sprintf("%s:%d", *host, *port)
 	gopts := []grpc.DialOption{grpc.WithBlock(), grpc.WithInsecure()}
 
-	log.Printf("start rs")
+	log.Printf("start rs: port=%v", *port)
 	rsvr := rserver.NewRServer()
 	if err := rsvr.Start(*port); err != nil {
 		log.Fatalf("start rs error: err=%+v", err)
@@ -121,6 +124,7 @@ func main() {
 		}
 	}()
 
+	log.Printf("create fp: master=%v, localAddr=%v, gopts=%+v", *master, localAddr, gopts)
 	fp := fproxy.NewFProxy(currentUid(), currentGid(), *master, localAddr, gopts)
 	rsvr.RegisterFProxy(fp)
 
@@ -132,21 +136,21 @@ func main() {
 		cfg.OpContext = ctx
 	}
 
-	log.Printf("mount fs")
+	log.Printf("mount fs: dir=%v", *dir)
 	isGracefulStop := false
 	fsvr := lfs.NewLFSServer(lfs.NewLFS(currentUid(), currentGid(), fp))
 	// Mount the file system.
-	mfs, err := fuse.Mount(dir, fsvr, &cfg)
+	mfs, err := fuse.Mount(*dir, fsvr, &cfg)
 	if err != nil {
 		log.Fatalf("mount fs error: dir=%v, err=%+v", dir, err)
 	}
 	defer func(_isGracefulStop *bool) {
 		if *_isGracefulStop {
-			log.Printf("no need to unmount fs")
+			log.Printf("no need to unmount fs: dir=%v", *dir)
 			return
 		}
-		log.Printf("unmount fs")
-		if err := fuse.Unmount(dir); err != nil {
+		log.Printf("unmount fs: dir=%v", *dir)
+		if err := fuse.Unmount(*dir); err != nil {
 			log.Fatalf("unmount fs error: dir=%v, err=%+v", dir, err)
 		}
 	}(&isGracefulStop)
--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -x -e
 
+testDir="x"
 testCMD="./test.sh"
 testLog="./tlog.txt"
 
 echo "====BUILD START===="
 go build -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 --oneline --date=local HEAD)\"' -X 'main.buildTime=\"$(date '+%Y/%m/%d %H:%M:%S')\"'"
 echo "====TEST START===="
-./pfs -testCmd=$testCMD -testLog=$testLog
+./pfs -dir=$testDir -testCmd=$testCMD -testLog=$testLog
 echo "====TEST LOG===="
 cat $testLog

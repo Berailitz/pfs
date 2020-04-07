9256f29ea64ee7a15098c5539d9aa16250fce801
Berailitz <admin@ohhere.xyz>
2020-04-07 15:03:10 +0800

    test(remote): add remote io test

--- test.sh
+++ test.sh
@@ -1,47 +1,104 @@
 #!/usr/bin/env bash
+
+inDir=''
+outDir=''
+ls='ls -l'
+
+function usage()
+{
+    echo "Usage of $0:"
+    echo -e "\t-h -help"
+    echo -e "\t-i -inDir=inDir"
+    echo -e "\t-o -outDir=outDir"
+    echo ""
+    echo "Test against basic filesystem operatiosn."
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | -help)
+            usage
+            exit
+            ;;
+        -i | --inDir)
+            inDir=$VALUE
+            ;;
+        -o | -outDir)
+            outDir=$VALUE
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$inDir" ]; then
+  echo 'no inDir specified.'
+  usage
+  exit 1
+fi
+if [ -z "$outDir" ]; then
+  echo 'no outDir specified.'
+  usage
+  exit 1
+fi
+
+echo "inDir is $inDir";
+echo "outDir is $outDir";
+
+# begin test
+set -x -e
+
+$ls $inDir
+$ls $outDir
+
+mkdir $inDir/a
+$ls $outDir
+
+echo "1stline" > $inDir/a/text
+cat $outDir/a/text
+
+echo "2ndline" > $inDir/a/text
+echo "3rdline" >> $inDir/a/text
+cat $outDir/a/text
+
+mkdir $inDir/a/adir
+$ls $outDir/a
+
+rmdir $inDir/a/adir
+$ls $outDir/a
+
+touch $inDir/einDirty
+$ls $outDir
+cat $outDir/einDirty
+
+mkdir $inDir/dir
+$ls $outDir
+
+rmdir $inDir/dir
+$ls $outDir
+
+rm $inDir/a/text
+$ls $outDir/a
+
+mv $inDir/einDirty $inDir/einDirty.new
+$ls $outDir
+
+mkdir $inDir/newdir
+mv $inDir/einDirty.new $inDir/newdir/new.txt
+$ls $outDir
+$ls $outDir/newdir
+
+mv $inDir/newdir/new.txt $inDir/back.txt
+$ls $outDir
+$ls $outDir/newdir
+
 set -x
-mp='x'
-ls $mp
 
-mkdir $mp/a
-ls $mp
-
-echo "1stline" > $mp/a/text
-cat $mp/a/text
-
-echo "2ndline" > $mp/a/text
-echo "3rdline" >> $mp/a/text
-cat $mp/a/text
-
-mkdir $mp/a/adir
-ls -l $mp/a
-
-rmdir $mp/a/adir
-ls -l $mp/a
-
-touch $mp/empty
-ls -l $mp
-cat $mp/empty
-
-mkdir $mp/dir
-ls -l $mp
-
-rmdir $mp/dir
-ls -l $mp
-
-rm $mp/a/text
-ls -l $mp/a
-
-mv $mp/empty $mp/empty.new
-ls -l $mp
-
-mkdir $mp/newdir
-mv $mp/empty.new $mp/newdir/new.txt
-ls -l $mp
-ls -l $mp/newdir
-
-mv $mp/newdir/new.txt $mp/back.txt
-ls -l $mp
-ls -l $mp/newdir
-
-umount $mp
+umount $inDir
+umount $outDir
--- testrun.sh
+++ testrun.sh
@@ -2,12 +2,12 @@
 set -x -e
 
 testDir="x"
-testCMD="./test.sh"
+testCMD="./test.sh -i=x -o=x"
 testLog="./tlog.txt"
 
 echo "====BUILD START===="
 go build -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 --oneline --date=local HEAD)\"' -X 'main.buildTime=\"$(date '+%Y/%m/%d %H:%M:%S')\"'"
 echo "====TEST START===="
-./pfs -dir=$testDir -testCmd=$testCMD -testLog=$testLog
+./pfs -dir="$testDir" -testCmd="$testCMD" -testLog="$testLog"
 echo "====TEST LOG===="
-cat $testLog
+cat "$testLog"

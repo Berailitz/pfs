2dd58a2623dc2194d4bef16787746fd83f073c96
Berailitz <admin@ohhere.xyz>
2020-04-25 15:34:39 +0800

    test(trun): update drun cmd

--- sample/drun.sh
+++ sample/drun.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 debugPort=18180
+debugCfg='x.yaml'
 
 set -x
 umount x
@@ -11,5 +12,5 @@ go build -gcflags "all=-N -l" -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 -
 echo "====BUILD END===="
 
 echo "====DEBUG START===="
-dlv --listen=:$debugPort --headless=true --api-version=2 --accept-multiclient exec ./pfs -- -dir=x
+dlv --listen=:$debugPort --headless=true --api-version=2 --accept-multiclient exec ./sample -- -cfg="$debugCfg"
 echo "====DEBUG END===="

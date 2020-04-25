f366a1ada17361290e36a88c87539902c2220b6b
Berailitz <admin@ohhere.xyz>
2020-04-25 15:32:32 +0800

    test(trun): update trun cmd

--- sample/trun.sh
+++ sample/trun.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -x -e
 
-testDir="x"
-testCMD="./test.sh -i=x -o=x"
+testCfg="sample.yaml"
 testLog="./tlog.txt"
 
 echo "====BUILD START===="
 go build -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 --oneline --date=local HEAD)\"' -X 'main.buildTime=\"$(date '+%Y/%m/%d %H:%M:%S')\"'"
 echo "====TEST START===="
-./pfs -dir="$testDir" -testCmd="$testCMD" -testLog="$testLog"
+./sample -cfg="$testCfg"
 echo "====TEST LOG===="
 cat "$testLog"

06f3ee11fe1df2e2f6d468cc12e0a2167af0c674
Berailitz <admin@ohhere.xyz>
2020-05-02 16:56:45 +0800

    feat(rs): Propose handle nil fp

--- rserver/rserver.go
+++ rserver/rserver.go
@@ -52,13 +52,24 @@ func (s *RServer) Ping(ctx context.Context, req *pb.PingRequest) (*pb.PingReply,
 	}, nil
 }
 
-func (s *RServer) Propose(ctx context.Context, req *pb.ProposeRequest) (*pb.ProposeReply, error) {
-	state, err := s.fp.AnswerProposal(ctx, req.Addr, &fbackend.Proposal{
-		ID:    req.ProposeID,
-		Typ:   req.ProposeType,
-		Key:   req.Key,
-		Value: req.Value,
-	})
+func (s *RServer) Propose(ctx context.Context, req *pb.ProposeRequest) (_ *pb.ProposeReply, err error) {
+	var state int64
+	if s.fp == nil {
+		state, err = s.ma.AnswerProposal(ctx, req.Addr, &fbackend.Proposal{
+			ID:    req.ProposeID,
+			Typ:   req.ProposeType,
+			Key:   req.Key,
+			Value: req.Value,
+		})
+	} else {
+		state, err = s.fp.AnswerProposal(ctx, req.Addr, &fbackend.Proposal{
+			ID:    req.ProposeID,
+			Typ:   req.ProposeType,
+			Key:   req.Key,
+			Value: req.Value,
+		})
+	}
+
 	var perr *pb.Error = &pb.Error{}
 	if err != nil {
 		perr = utility.ToPbErr(err)

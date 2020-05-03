f4204a89fe8c1fbdca5734a5280a0bee6aa41efd
Berailitz <admin@ohhere.xyz>
2020-05-03 16:39:11 +0800

    feat(sp): drun use cfg

--- sample/drun.sh
+++ sample/drun.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 debugPort=18180
-debugCfg='x.yaml'
+runCfg="$1"
 
 set -x
 umount x
+umount y
 
 set -x -e
+
+[ -z "$runCfg" ] && echo "empty cfg path, exiting" && exit 1
+
 echo "====BUILD START===="
 go build -gcflags "all=-N -l" -ldflags="-X 'main.gitCommit=\"$(git rev-list -1 --oneline --date=local HEAD)\"' -X 'main.buildTime=\"$(date '+%Y/%m/%d %H:%M:%S')\"'" 2>&1
 echo "====BUILD END===="
 
 echo "====DEBUG START===="
-dlv --listen=:$debugPort --headless=true --api-version=2 --accept-multiclient exec ./sample -- -cfg="$debugCfg" 2>&1
+dlv --listen=:$debugPort --headless=true --api-version=2 --accept-multiclient exec ./sample -- -cfg="$runCfg" 2>&1
 echo "====DEBUG END===="

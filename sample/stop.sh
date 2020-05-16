9a1666f3982ab8b66c5b47ca2d6358e080f51061
Berailitz <admin@ohhere.xyz>
2020-05-17 01:36:28 +0800

    feat(sp): add z

--- sample/stop.sh
+++ sample/stop.sh
@@ -6,6 +6,7 @@ debugPort=18180
 
 umount -f x
 umount -f y
+umount -f z
 
 kill "$(ps -ef|grep "dlv --listen=:$debugPort"|head -n 1|awk '{print $2}')"
 kill "$(ps -ef|grep "sample"|head -n 1|awk '{print $2}')"
